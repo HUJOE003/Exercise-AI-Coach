ff0544c1069dc4f64b2f9ca3695b665628075551
HUJOE003 <hpandise@calpoly.edu>
2024-09-11 14:03:38 +0530

    fixed nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,19 @@
-/** @type {import('next').NextConfig} */
-const withPwa = require("next-pwa")({
-    dest: "public",
-    swSrc: "/public/custom-service-worker.js",
-    register: true,
-    skipWaiting: true,
-    buildExcludes: [/middleware-manifest.json$/, /app-build-manifest.json$/],
-    disable: process.env.NODE_ENV === "development",
-  });
-  
-  const nextConfig = withPwa({
-    reactStrictMode: true,
-  });
-  
-  module.exports = nextConfig;
+// next.config.mjs
+
+import withPwa from "next-pwa";
+
+const pwaConfig = withPwa({
+  dest: "public",
+  swSrc: "/public/custom-service-worker.js",
+  register: true,
+  skipWaiting: true,
+  buildExcludes: [/middleware-manifest.json$/, /app-build-manifest.json$/],
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  ...pwaConfig,
+};
+
+export default nextConfig;
